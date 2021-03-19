7d1a724d12fdc83ae27c5575ac78a6ade0373248
asadbektolipov <80939762+asadbektolipov@users.noreply.github.com>
2021-03-19 12:41:15 +0500

    Add files via upload

---
+++ Youtube-Downloader-Bot-master/config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("1666963170:AAGIUi5Jr_taVOmjLPs5h-G76_G4auBxym4")
+APP_ID = int(os.environ.get("3082743"))
+API_HASH = os.environ.get("e6e15049f1d0a70801b4c51f54d9471f")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
---
+++ Youtube-Downloader-Bot-master/plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Bizning kanal", url="https://t.me/technoliteuz")],
+        [InlineKeyboardButton(
+            "Murojaat uchun", url="https://t.me/technoliteuz")]
+    ])
+    welcomed = f"Salom <b>{message.from_user.first_name}</b>\n/ yordam kerak bo'lsa shu buyruqni bosing"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
